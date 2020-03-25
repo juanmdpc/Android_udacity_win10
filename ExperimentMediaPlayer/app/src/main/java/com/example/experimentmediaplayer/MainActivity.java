df47295dd228fa3dea2e52195f5b9a18434894af
fisherman <juanmdpc@gmail.com>
2020-03-24 22:17:25 -0500

    Multiscreen Apps finished

--- ExperimentMediaPlayer/app/src/main/java/com/example/experimentmediaplayer/MainActivity.java
+++ ExperimentMediaPlayer/app/src/main/java/com/example/experimentmediaplayer/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -37,6 +38,19 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View v)
             {
                 mediaplayer.pause();
+            }
+        });
+
+        mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+        {
+            @Override
+            public void onCompletion(MediaPlayer mp)
+            {
+                Toast.makeText(
+                        MainActivity.this,
+                        "I'm done",
+                        Toast.LENGTH_SHORT)
+                        .show();
             }
         });
     }
